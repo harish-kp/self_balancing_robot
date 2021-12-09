3f8c1ab8c423d8891f5a9af154a2462de8b9abea
Harish Kp <kperatchi@gmail.com>
2021-12-09 14:32:22 +1100

    Testing new methods

--- src/SelfBalance_withLQR.py
+++ src/SelfBalance_withLQR.py
@@ -11,9 +11,9 @@ from std_msgs.msg import Float32
 A = np.array([[0,1],[47.77,0]])
 B = np.array([[  0  ],
        [ 10.52]])
-Q = np.array([[ 1000,   0],
-       [  0, 10000]])
-R = 1000.0
+Q = np.array([[ 100,   0],
+       [  0, 1000]])
+R = 0.0001
 K,S,e = lqr(A,B,Q,R)
 
 cmd_vel = "/cmd_vel"
@@ -28,10 +28,10 @@ class SelfBalanceLQR:
         self.subscriber2 = rospy.Subscriber(q_topic,Float32,self.callback_q)
         self.subscriber3= rospy.Subscriber(r_topic,Float32,self.callback_r)
         self.pub1 = rospy.Publisher(Yaw_Topic,Float32,queue_size =1)
-        self.xvelMin= 0.0
-        self.xvelMax = 0.01
-        self.yMin = -0.0001
-        self.yMax = 0.0001
+        self.xvelMin= -0.01
+        self.xvelMax = 0
+        self.yMin = -0.1
+        self.yMax = 0.1
         self.y_ = 0.0
         self.Q = Q
         self.R = R
@@ -39,33 +39,33 @@ class SelfBalanceLQR:
         self.K,self.S,self.e = lqr(A,B,self.Q,self.R)
     def callback(self,data):
         self.K,self.S,self.e = lqr(A,B,self.Q,self.R)
-        y = data.orientation.z
+        y = data.orientation.y*(180/3.1415)
         # print (y)
-        if y>self.yMax:
-            y = self.yMax
-        elif y<self.yMin:
-            y = self.yMin
-        data.orientation.w = y
+        if y > self.yMax:
+            self.yMax = y
+        elif y < self.yMin:
+            self.yMin = y
+        # data.orientation.w = y
         vel = Twist()
         diff_yaw = y-self.y_
         np_x = np.array([[y],[diff_yaw]])
         xvel = self.K.dot(np_x)[0,0]
-        if xvel>self.xvelMax:
-            xvel = self.xvelMax
-        elif xvel<self.xvelMin:
+        if xvel>=self.xvelMax:
             xvel = self.xvelMin
+        elif xvel<=self.xvelMin:
+            xvel = self.xvelMax
         vel.linear.x = xvel
         vel.linear.y = 0
         vel.linear.z = 0
         vel.angular.x = 0
-        vel.angular.y = y
+        vel.angular.y = 0
         vel.angular.z = 0
         self.pub.publish(vel)
         # print ("Max vel " + str(self.xvelMax) + " & Min vel " + str(self.xvelMin) + " Max y " + str(self.yMax*180/3.1416) +" & Min y" + str(self.yMin*180/3.1416))
         print ("Velocity "+ str(xvel)+ " & yaw " + str(y))
         self.y_ = diff_yaw
-        # data.orientation.w = diff_yaw
-        self.pub1.publish(data.orientation.w)
+        # data.orientation.w = y
+        self.pub1.publish(data.orientation.y)
     def callback_q(self,data):
         q = data.data
         self.Q = q*Q
